2c679240d2174297203794ab7c2357b58a787c35
Krzysiek <krzysztof.orawski@wp.pl>
2025-10-29 19:38:15 +0100

    pask dan

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALL_COUNT = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < BALL_COUNT; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
