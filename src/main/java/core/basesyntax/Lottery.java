b66adf2db6c2e7fa9cdfd1cbe3dfdbe89d66da91
Krzysiek <krzysztof.orawski@wp.pl>
2025-10-29 19:44:23 +0100

    pask dan

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER)+1);
     }
 }
