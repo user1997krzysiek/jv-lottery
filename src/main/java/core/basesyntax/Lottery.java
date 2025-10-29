cc00e516d07cb28ae0a22eaf9e4fcba9bb50f08c
Krzysiek <krzysztof.orawski@wp.pl>
2025-10-29 19:47:26 +0100

    pask dan

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -8,6 +8,6 @@ public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER)+1);
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER) + 1);
     }
 }
